10315d51be41e31be3ec26cc8bfafde6fbdc86e7
Roshankumar0808 <roshananand2014@gmail.com>
2025-02-01 12:03:12 +0530

    JPA1

---
+++ src/test/java/com/Jpa_Hybernate/Jpa_Hybernate/JpaHybernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jpa_Hybernate.Jpa_Hybernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHybernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
