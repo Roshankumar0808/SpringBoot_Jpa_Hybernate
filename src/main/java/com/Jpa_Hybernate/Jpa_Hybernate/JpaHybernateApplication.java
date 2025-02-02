ddd896f4a6e70d7d340c4034f9671ab4f3ff9441
Roshankumar0808 <roshananand2014@gmail.com>
2025-02-02 23:59:05 +0530

    Steps from jpql to sql conversion (Hybernate)

--- src/main/java/com/Jpa_Hybernate/Jpa_Hybernate/JpaHybernateApplication.java
+++ src/main/java/com/Jpa_Hybernate/Jpa_Hybernate/JpaHybernateApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaHybernateApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JpaHybernateApplication.class, args);
 	}
-
+//	Final Summary
+//	JPQL query is written in Spring Data JPA.
+//	Hibernate converts JPQL into SQL.
+//	SQL query executes in the database engine.
+//	The database returns the result.
+//	Hibernate maps the result to Java entities.
+//	Saving an entity triggers an SQL INSERT statement.
+//	Hibernate manages transactions and persists data.
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO test.product_table (price, quantity, sku, title_x)
+VALUES
+    (84, 4, 'Parlee', 'Parlee Biscuits'),
+    (66, 4, 'Mariee', 'Marie Biscuits'),
+    (44, 4, 'GooodDay', 'Gooday Biscuits'),
+    (49, 4, 'Britnannia', 'Britannia Biscuits'),
+    (41, 4, 'Cadburry', 'Oreo Biscuits');
